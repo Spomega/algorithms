7c8c046fc3f1dd0f86627dfbdc0eef6a63240ad4
joseph.davis <joseph.davis@thinsurance.de>
2024-09-26 22:34:06 +0200

    added gitignore

--- php/vendor/composer/autoload_real.php
+++ php/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit06cfaf98a7c9978f884b8aa286e11854
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit06cfaf98a7c9978f884b8aa286e11854::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
